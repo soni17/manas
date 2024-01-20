02b7bc293b9788b18f0b0886498923c4f6d1804f
Enderson Isai <elb004@gmail.com>
2024-01-20 17:14:52 -0500

    add comments to flatpaks

--- installer.sh
+++ installer.sh
@@ -8,8 +8,11 @@ sudo apt autoremove -y
 # install packages from Debian repo
 sudo apt install -y git adb wget curl flatpak gnome-screenshot gnome-software gnome-software-plugin-flatpak cmatrix gtkhash vlc gnome-clocks rhythmbox shotcut chromium webcamoid timeshift gnome-disk-utility gnome-package-updater package-update-indicator caja-admin caja-image-converter caja-open-terminal caja-rename gdebi mozo gufw redshift blueman htop btop ncdu gucharmap dconf-cli ayatana-indicator-application ayatana-indicator-common ayatana-indicator-messages ayatana-indicator-notifications ayatana-indicator-power ayatana-indicator-printers ayatana-indicator-sound mate-indicator-applet mate-indicator-applet-common libreoffice-writer libreoffice-gtk3 lsb-release mate-dock-applet neofetch lshw tldr bat transmission 
 
-#configue flatpak
+# configue flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# install flatpak apps
 flatpak install flathub org.gnome.SimpleScan -y
 flatpak install flathub io.podman_desktop.PodmanDesktop -y
 flatpak install flathub com.jgraph.drawio.desktop -y
+
