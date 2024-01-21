a1a16363958e2472b8b16c86682a190280109c27
Enderson Isai <elb004@gmail.com>
2024-01-20 19:17:51 -0500

    uninstall unused packages

--- installer.sh
+++ installer.sh
@@ -4,6 +4,8 @@ sudo apt full-upgrade &&
 sudo apt dist-upgrade && 
 sudo apt autoremove -y
 
+# uninstall unused packages
+sudo apt purge gnome-keyring fonts-noto seahorse firefox-esr* gimp* goldendict mlterm mlterm-tiny xterm xiterm+thai libreoffice* mysql-common orca
 
 # install packages from Debian repo
 sudo apt install -y git adb wget curl flatpak gnome-screenshot gnome-software gnome-software-plugin-flatpak cmatrix gtkhash vlc gnome-clocks rhythmbox shotcut chromium webcamoid timeshift gnome-disk-utility gnome-package-updater package-update-indicator caja-admin caja-image-converter caja-open-terminal caja-rename gdebi mozo gufw redshift blueman htop btop ncdu gucharmap dconf-cli ayatana-indicator-application ayatana-indicator-common ayatana-indicator-messages ayatana-indicator-notifications ayatana-indicator-power ayatana-indicator-printers ayatana-indicator-sound mate-indicator-applet mate-indicator-applet-common libreoffice-writer libreoffice-gtk3 lsb-release mate-dock-applet neofetch lshw tldr bat transmission 
