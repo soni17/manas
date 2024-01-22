30a8786104cd0dd50593fdfb5f9d42c82380b78d
Enderson Isai <elb004@gmail.com>
2024-01-21 19:14:12 -0500

    avoid prompts during installation with  -y flag

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 # update and upgrade
-sudo apt update && 
-sudo apt full-upgrade &&  
-sudo apt dist-upgrade && 
+sudo apt update -y
+sudo apt full-upgrade -y  
+sudo apt dist-upgrade -y
 sudo apt autoremove -y
 
 # uninstall unused packages
-sudo apt purge gnome-keyring fonts-noto seahorse firefox-esr* gimp* goldendict mlterm mlterm-tiny xterm xiterm+thai libreoffice* mysql-common orca mate-applet-brisk-menu mate-applets mate-utils
+sudo apt purge gnome-keyring fonts-noto seahorse firefox-esr* gimp* goldendict mlterm mlterm-tiny xterm xiterm+thai libreoffice* mysql-common orca mate-applet-brisk-menu mate-applets mate-utils -y
 
 # install packages from Debian repo
-sudo apt install -y git adb wget curl flatpak gnome-screenshot gnome-software gnome-software-plugin-flatpak cmatrix gtkhash vlc gnome-clocks rhythmbox shotcut chromium webcamoid timeshift gnome-disk-utility gnome-package-updater package-update-indicator caja-admin caja-image-converter caja-open-terminal caja-rename gdebi mozo gufw redshift blueman htop btop ncdu gucharmap dconf-cli ayatana-indicator-application ayatana-indicator-common ayatana-indicator-messages ayatana-indicator-notifications ayatana-indicator-power ayatana-indicator-printers ayatana-indicator-sound mate-indicator-applet mate-indicator-applet-common libreoffice-writer libreoffice-gtk3 lsb-release mate-dock-applet neofetch lshw tldr bat transmission 
+sudo apt install git adb wget curl flatpak gnome-screenshot gnome-software gnome-software-plugin-flatpak cmatrix gtkhash vlc gnome-clocks rhythmbox shotcut chromium webcamoid timeshift gnome-disk-utility gnome-package-updater package-update-indicator caja-admin caja-image-converter caja-open-terminal caja-rename gdebi mozo gufw redshift blueman htop btop ncdu gucharmap dconf-cli ayatana-indicator-application ayatana-indicator-common ayatana-indicator-messages ayatana-indicator-notifications ayatana-indicator-power ayatana-indicator-printers ayatana-indicator-sound mate-indicator-applet mate-indicator-applet-common libreoffice-writer libreoffice-gtk3 lsb-release mate-dock-applet neofetch lshw tldr bat transmission -y
 
 # configue flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -19,5 +19,5 @@ flatpak install flathub io.podman_desktop.PodmanDesktop -y
 flatpak install flathub com.jgraph.drawio.desktop -y
 
 # install packages from files
-sudo apt install ./packages/pinta_1.6-2.1_all.deb
-sudo apt install ./packages/dbgate-latest.deb
+sudo apt install ./packages/pinta_1.6-2.1_all.deb -y
+sudo apt install ./packages/dbgate-latest.deb -y
