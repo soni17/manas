018efc785e071026985efa488c175ad6e4a309da
Enderson Isai <elb004@gmail.com>
2024-01-24 19:58:59 -0500

    show fewer logs during boot screen

--- installer.sh
+++ installer.sh
@@ -32,3 +32,9 @@ cp wallpapers/retro80s.jpg ~/Pictures
 
 # settings
 sudo ufw enable
+
+# reduce timeout to 3 seconds during boot
+# minimize splash screen at boot to show fewer logs
+sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/" /etc/default/grub
+sudo sed -i "s/splash/unsplash loglevel=3/" /etc/default/grub
+sudo update-grub
