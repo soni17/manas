663bff8ab1332a5207fdb9d4cc3c5daaaa5d55ab
Enderson Isai <elb004@gmail.com>
2024-01-25 18:05:27 -0500

    fix for netowork-monitor applet

--- installer.sh
+++ installer.sh
@@ -38,3 +38,6 @@ sudo ufw enable
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/" /etc/default/grub
 sudo sed -i "s/splash/unsplash loglevel=3/" /etc/default/grub
 sudo update-grub
+
+# fix for network-monitor applet not showing in panel
+sudo sed -i "s/Exec=nm-applet/Exec=nm-applet --indicator/" /etc/xdg/autostart/nm-applet.desktop
