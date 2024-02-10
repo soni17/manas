ad9afbb7769ee9b34988a32c9d34a34b035d9e95
Enderson Isai <elb004@gmail.com>
2024-02-10 14:59:12 -0500

    add ngrok repo and install ngrok

--- installer.sh
+++ installer.sh
@@ -76,6 +76,12 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker une
 
+# install ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+sudo apt update
+sudo apt install -y ngrok
+
 
 # ----------------------settings-------------------------------
 
