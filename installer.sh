a6daf91aeb359bc4cff0c5b6c3e482e029e61d54
Enderson Isai <elb004@gmail.com>
2024-02-14 17:12:19 -0500

    add beekeeper-studio repo and install

--- installer.sh
+++ installer.sh
@@ -96,6 +96,13 @@ EOF
 sudo apt update
 sudo apt install librewolf -y
 
+# install Beekeeper Studio and repo
+curl -fsSL https://deb.beekeeperstudio.io/beekeeper.key | sudo gpg --dearmor --output /usr/share/keyrings/beekeeper.gpg
+sudo chmod go+r /usr/share/keyrings/beekeeper.gpg
+echo "deb [signed-by=/usr/share/keyrings/beekeeper.gpg] https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list > /dev/null
+sudo apt update
+sudo apt install beekeeper-studio -y
+
 
 # ----------------------settings-------------------------------
 
