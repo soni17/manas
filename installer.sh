91a589643e2d5a996506a52ffbd8451aeaa68960
Enderson Isai <elb004@gmail.com>
2024-06-02 12:34:46 -0400

    move librewolf installer into separate file

--- installer.sh
+++ installer.sh
@@ -35,20 +35,6 @@ sudo apt install -y ttf-mscorefonts-installer
 sudo bash settings/ttf-vista-fonts-installer.sh
 sudo bash settings/ttf-ms-tahoma-installer.sh
 
-# install librewolf and repo
-distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-sudo apt update
-sudo apt install librewolf -y
-
 
 # ----------------------settings-------------------------------
 
