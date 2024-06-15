c0247560dcc859b1217e533bc6b1e3e8bbb9821b
Enderson Isai <elb004@gmail.com>
2024-06-14 21:16:03 -0400

    move apps from Debian repo to separate file

--- installer.sh
+++ installer.sh
@@ -17,9 +17,6 @@ sudo apt install -y \
 # run installers
 for script in ./installers/*.sh; do source $script; done
 
-# install packages from Debian repo
-sudo apt install -y adb gnome-screenshot cmatrix gtkhash vlc gnome-clocks rhythmbox shotcut webcamoid timeshift gnome-disk-utility gnome-package-updater package-update-indicator caja-admin caja-image-converter caja-open-terminal caja-rename gdebi mozo redshift blueman htop btop ncdu gucharmap lsb-release neofetch lshw tldr bat transmission 
-
 # update and cleanup
 sudo apt update -y
 sudo apt full-upgrade -y
---
+++ installers/apps-various.sh
@@ -0,0 +1,29 @@
+sudo apt install -y \
+  btop \
+  ncdu \
+  neofetch \
+  lshw \
+  tldr \
+  bat \
+  lsb-release \
+  gnome-disk-utility \
+  gnome-package-updater \
+  package-update-indicator \
+  caja-admin \
+  caja-image-converter \
+  caja-open-terminal \
+  caja-rename \
+  gdebi \
+  mozo \
+  blueman \
+  gucharmap \
+  transmission \
+  vlc \
+  shotcut \
+  rhythmbox \
+  webcamoid \
+  gtkhash \
+  timeshift \
+  gnome-clocks \
+  gnome-screenshot \
+  cmatrix 
