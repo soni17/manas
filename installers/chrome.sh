fe7161bece7adfd1e516a341600b8011a345f281
Enderson Isai <elb004@gmail.com>
2024-06-19 21:24:40 -0400

    download google chrome directly

--- installers/chrome.sh
+++ installers/chrome.sh
@@ -1,4 +1,6 @@
-sudo apt install -y ./packages/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 
 # fix for slow startup
 sudo sed -i "s/StartupNotify=true//" /usr/share/applications/google-chrome.desktop
