78894d78e4d1e89c594f342d54b72301c81b5df4
Enderson Isai <elb004@gmail.com>
2024-06-20 10:15:41 -0400

    move theme files to a separate folder

--- installers/theme.sh
+++ installers/theme.sh
@@ -18,33 +18,33 @@ sudo bash packages/ttf-ms-tahoma-installer.sh
 sudo apt purge -y fontforge
 
 # load settings
-dconf load / < configs/theme-settings
+dconf load / < theme/theme-settings
 
 # wallpaper
-cp configs/wallpapers/* ~/Pictures
+cp theme/wallpapers/* ~/Pictures
 hme=~
 dconf write /org/mate/desktop/background/picture-filename "'$hme/Pictures/beach-rocks.jpg'"
 
 # desktop icons
-cp configs/desktop-icons/* ~/Desktop
+cp theme/desktop-icons/* ~/Desktop
 
 # theme file
 mkdir -p ~/.themes/classic
-cp configs/index.theme ~/.themes/classic/index.theme
+cp theme/index.theme ~/.themes/classic/index.theme
 
 # modify panel menus
 if [ -d ~/.config/menus ]; then
   rm -r ~/.config/menus
 fi
 mkdir -p ~/.config/menus
-cp -r configs/panel-menus/* ~/.config/menus
+cp -r theme/panel-menus/* ~/.config/menus
 
 # modify panel apps
 if [ -d ~/.local/share/applications ]; then
   rm -r ~/.local/share/applications
 fi
 mkdir -p ~/.local/share/applications
-cp -r configs/panel-apps/* ~/.local/share/applications
+cp -r theme/panel-apps/* ~/.local/share/applications
 
 # other settings
 xdg-mime default gnome-disk-image-mounter.desktop application/x-cd-image
@@ -52,4 +52,4 @@ xdg-mime default gnome-disk-image-mounter.desktop application/x-cd-image
 # set screen temperature
 sudo apt install -y redshift
 mkdir -p ~/.config/autostart
-cp configs/redshift.desktop ~/.config/autostart
+cp theme/redshift.desktop ~/.config/autostart
