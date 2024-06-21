48d40961ba418771e7665af9c81641acb954cef2
Enderson Isai <elb004@gmail.com>
2024-06-21 16:37:02 -0400

    fix download url for usbimager

--- installers/usb-imager.sh
+++ installers/usb-imager.sh
@@ -1,6 +1,6 @@
 # TO DO: download latest version
 
 # download and install
-wget https://gitlab.com/bztsrc/usbimager/-/blob/binaries/usbimager_1.0.10-amd64.deb
+wget https://gitlab.com/bztsrc/usbimager/-/raw/binaries/usbimager_1.0.10-amd64.deb
 sudo apt install -y ./usbimager_1.0.10-amd64.deb
 rm usbimager_1.0.10-amd64.deb
