8aee3557bb4333f1c213182494a4f11c8a376725
Enderson Isai <elb004@gmail.com>
2024-06-24 21:19:14 -0400

    set chromium as default browser

--- installers/chromium.sh
+++ installers/chromium.sh
@@ -2,3 +2,6 @@ sudo apt install -y chromium
 
 # fix for slow startup
 sudo sed -i "s/StartupNotify=true//" /usr/share/applications/chromium.desktop
+
+# set as default browser
+xdg-settings set default-web-browser chromium.desktop
