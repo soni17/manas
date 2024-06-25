27202ccd21daac67cdcf238a973516b93f6a2b9e
Enderson Isai <elb004@gmail.com>
2024-06-24 21:42:53 -0400

    add dependencies for librewolf

--- installers/librewolf.sh
+++ installers/librewolf.sh
@@ -1,5 +1,13 @@
 # https://librewolf.net/installation/debian/
 
+# dependencies
+sudo apt install -y \
+  wget \
+  gnupg \
+  lsb-release \
+  apt-transport-https \
+  ca-certificates
+
 # add repository
 distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
