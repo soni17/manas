611bc980c9d9886317b3b3b3f4c2196e8f8d708a
Enderson Isai <elb004@gmail.com>
2024-06-26 22:41:57 -0400

    refactor docker installer

--- installers/docker.sh
+++ installers/docker.sh
@@ -11,10 +11,10 @@ sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # add repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ARCH=$(dpkg --print-architecture)
+OS_CODENAME=$(. /etc/os-release && echo $VERSION_CODENAME)
+REPO="deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $OS_CODENAME stable"
+echo $REPO | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install docker and extensions
 sudo apt update -y
