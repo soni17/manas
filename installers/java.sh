e8db3f6a8e564165f2d82d8aaf9d3ef7385a8d00
Enderson Isai <elb004@gmail.com>
2024-07-01 13:37:41 -0400

    various config changes

--- installers/java.sh
+++ installers/java.sh
@@ -1,5 +1,5 @@
 # install latest version of Java (openjdk)
 asdf plugin add java
 OPENJDK=$(asdf list all java | grep openjdk | grep -v + | tail -1)
-asdf install $OPENJDK
-asdf global $OPENJDK
+asdf install java $OPENJDK
+asdf global java $OPENJDK
