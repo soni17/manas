393bc66fd42779ddaf9ad002e251812fabd21405
Enderson Isai <elb004@gmail.com>
2024-07-21 19:52:15 -0400

    refactor cli tools installer

---
+++ installers/cli-tools.sh
@@ -0,0 +1,15 @@
+# install cli tools from debian repository
+sudo apt install -y \
+  tldr \
+  bat \
+  lsd \
+  btop \
+  ncdu \
+  fzf \
+  tmux \
+  speedtest-cli \
+  lsb-release \
+  neofetch \
+  cpufetch \
+  podman \
+  lshw
