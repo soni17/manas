468e9a3c0fdab134b815b07e218d99fefb99f3c5
Enderson Isai <elb004@gmail.com>
2024-07-21 20:40:30 -0400

    stop installation if debian version is older than 12

--- run-commands/check-os.sh
+++ run-commands/check-os.sh
@@ -1,9 +1,14 @@
 . /etc/os-release
 distro=$ID
 desktop=$DESKTOP_SESSION
+version=$VERSION_ID
 
 if [ "$distro" != "debian" ] || [ "$desktop" != "mate" ]; then
   echo "$(tput setaf 1)You are not running Debian with Mate desktop environment."
+  echo "$(tput setaf 1)Installation stopped."
+  exit 1
+elif [ $version -lt 12 ]; then
+  echo "$(tput setaf 1)Your version of Debian is older than 12"
   echo "$(tput setaf 1)Installation stopped."
   exit 1
 fi
