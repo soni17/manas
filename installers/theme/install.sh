3e485736e5311af7a429ad936360732a752575f0
Enderson Isai <elb004@gmail.com>
2024-07-23 03:06:56 -0400

    update file paths in theme installer

--- installers/theme/install.sh
+++ installers/theme/install.sh
@@ -1,6 +1,6 @@
 # install icon-themes
-sudo apt install -y ./theme/icon-themes/humanity-icon-theme_0.6.16_all.deb
-sudo apt install -y ./theme/icon-themes/ubuntu-mono_14.04_all.deb
+sudo apt install -y ./installers/theme/icon-themes/humanity-icon-theme_0.6.16_all.deb
+sudo apt install -y ./installers/theme/icon-themes/ubuntu-mono_14.04_all.deb
 
 # install crosextra fonts
 sudo apt install -y \
@@ -8,32 +8,32 @@ sudo apt install -y \
   fonts-crosextra-carlito
 
 # install ubuntu fonts
-sudo apt install -y ./theme/fonts/ttf-ubuntu-font-family_0.80-0ubuntu6_all.deb
+sudo apt install -y ./installers/theme/fonts/ttf-ubuntu-font-family_0.80-0ubuntu6_all.deb
 
 # install microsoft fonts
 sudo apt install -y ttf-mscorefonts-installer
-sudo bash theme/fonts/ttf-vista-fonts-installer.sh
-sudo bash theme/fonts/ttf-ms-tahoma-installer.sh
+sudo bash /installers/theme/fonts/ttf-vista-fonts-installer.sh
+sudo bash /installers/theme/fonts/ttf-ms-tahoma-installer.sh
 
 # wallpapers
 sudo rm -r /usr/share/backgrounds/*
-sudo cp theme/wallpapers/* /usr/share/backgrounds
+sudo cp installers/theme/wallpapers/* /usr/share/backgrounds
 sudo rm -r /usr/share/mate-background-properties/*
-sudo cp theme/settings/backgrounds.xml /usr/share/mate-background-properties
+sudo cp installers/theme/settings/backgrounds.xml /usr/share/mate-background-properties
 dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/beach-rocks.jpg'"
 
 # desktop icons
-cp theme/desktop-icons/* ~/Desktop
+cp installers/theme/desktop-icons/* ~/Desktop
 
 # modify panel menus
 rm -r ~/.config/menus
 mkdir -p ~/.config/menus
-cp theme/settings/mate-applications.menu ~/.config/menus
+cp installers/theme/settings/mate-applications.menu ~/.config/menus
 
 # modify panel apps
 rm -r ~/.local/share/applications
 mkdir -p ~/.local/share/applications
-cp -r theme/panel-apps/* ~/.local/share/applications
+cp -r installers/theme/panel-apps/* ~/.local/share/applications
 
 # install panel applets
 sudo apt install -y \
@@ -52,16 +52,16 @@ sudo apt install -y \
 sudo sed -i "s/Exec=nm-applet/Exec=nm-applet --indicator/" /etc/xdg/autostart/nm-applet.desktop
 
 # load theme settings
-dconf load / < theme/settings/theme-settings
+dconf load / < installers/theme/settings/theme-settings
 
 # theme file
 sudo mkdir -p /usr/share/themes/Manas
-sudo cp theme/settings/index.theme /usr/share/themes/Manas
+sudo cp installers/theme/settings/index.theme /usr/share/themes/Manas
 
 # set screen temperature
 sudo apt install -y redshift
 mkdir -p ~/.config/autostart
-cp theme/settings/redshift.desktop ~/.config/autostart
+cp installers/theme/settings/redshift.desktop ~/.config/autostart
 
 # install caja extensions
 sudo apt install -y \
