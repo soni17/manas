41be1608e9ec817b5ab536fa915c459fe4daf134
Enderson Isai <elb004@gmail.com>
2024-07-25 00:35:01 -0400

    disable recent documents in panel menu

--- installers/theme/install.sh
+++ installers/theme/install.sh
@@ -70,6 +70,10 @@ sudo apt install -y \
   caja-open-terminal \
   caja-rename
 
+# disable recent documents in menu
+rm ~/.local/share/recently-used*
+mkdir -p ~/.local/share/recently-used.xbel
+
 # minimize splash screen at boot
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/" /etc/default/grub
 sudo sed -i "s/splash/unsplash loglevel=3/" /etc/default/grub
