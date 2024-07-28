359368829a8a58f7b4f682d4720d6ed1abb13be8
Enderson Isai <elb004@gmail.com>
2024-07-28 13:26:00 -0400

    add comment to pinta installer

--- installers/pinta/install.sh
+++ installers/pinta/install.sh
@@ -3,4 +3,6 @@ flatpak install flathub --assumeyes --system com.github.PintaProject.Pinta
 # settings file
 mkdir -p ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
 cp installers/pinta/settings.xml ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
+
+# panel icon
 cp installers/pinta/com.github.PintaProject.Pinta.desktop ~/.local/share/applications
