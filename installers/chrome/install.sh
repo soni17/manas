a92acc00814991717bcbaece7a3df2c0f39c1c65
Enderson Isai <elb004@gmail.com>
2024-07-28 13:25:06 -0400

    move chrome installer in separate folder

--- installers/chrome/install.sh
+++ installers/chrome/install.sh
@@ -4,3 +4,6 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
+
+# panel icon
+cp installers/chrome/google-chrome.desktop ~/.local/share/applications
