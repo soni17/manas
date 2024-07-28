6507830f7a1fe99b0f7130d15a3b58e69cba83cf
Enderson Isai <elb004@gmail.com>
2024-07-28 13:33:06 -0400

    move chromium installer to separate folder

--- installers/chromium/install.sh
+++ installers/chromium/install.sh
@@ -1,4 +1,7 @@
 sudo apt install -y chromium
 
+# panel icon
+cp installers/chromium/chromium.desktop ~/.local/share/applications
+
 # set as default browser
 xdg-settings set default-web-browser chromium.desktop
