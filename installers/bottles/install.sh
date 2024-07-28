de4b941b44cca4edbcf2020415b5cbe2d0b556f5
Enderson Isai <elb004@gmail.com>
2024-07-28 14:11:11 -0400

    move bottles installer in separate folder

--- installers/bottles/install.sh
+++ installers/bottles/install.sh
@@ -1,4 +1,8 @@
+# install bottles
 flatpak install flathub --assumeyes --system com.usebottles.bottles
 
 # allow bottles to create desktop entries
 flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
+
+# panel icon
+cp installers/bottles/com.usebottles.bottles* ~/.local/share/applications
