67bcc738b1e6bcd279a0b894fc5d7c8f3856a453
Enderson Isai <elb004@gmail.com>
2024-07-28 16:42:32 -0400

    move installers in categorized folders

--- run-commands/run-installers.sh
+++ run-commands/run-installers.sh
@@ -1,5 +1,11 @@
-# run installers inside the installers folder
-for script in ./installers/*.sh; do source $script; done
+# run installers in apps-system
+for script in ./apps-system/*.sh; do source $script; done
+for script in ./apps-system/*/*.sh; do source $script; done
 
-# run installers in subfolders
-for script in ./installers/*/*.sh; do source $script; done
+# run installers in apps-development
+for script in ./apps-development/*.sh; do source $script; done
+for script in ./apps-development/*/*.sh; do source $script; done
+
+# run installers in apps-other
+for script in ./apps-other/*.sh; do source $script; done
+for script in ./apps-other/*/*.sh; do source $script; done
