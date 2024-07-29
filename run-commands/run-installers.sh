ad4ce45c67ee61fff9c8dee00eeb70302841058d
Enderson Isai <elb004@gmail.com>
2024-07-29 15:53:13 -0400

    move installers in their own folder

--- run-commands/run-installers.sh
+++ run-commands/run-installers.sh
@@ -1,11 +1,8 @@
 # run installers in apps-system
-for script in ./apps-system/*.sh; do source $script; done
-for script in ./apps-system/*/*.sh; do source $script; done
+for script in ./apps-system/*/install.sh; do source $script; done
 
 # run installers in apps-development
-for script in ./apps-development/*.sh; do source $script; done
-for script in ./apps-development/*/*.sh; do source $script; done
+for script in ./apps-development/*/install.sh; do source $script; done
 
 # run installers in apps-other
-for script in ./apps-other/*.sh; do source $script; done
-for script in ./apps-other/*/*.sh; do source $script; done
+for script in ./apps-other/*/install.sh; do source $script; done
