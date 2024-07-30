7b8a3568fa349a841b71e94a2ae3fc220f1e854c
Enderson Isai <elb004@gmail.com>
2024-07-30 01:35:30 -0400

    uninstaller for onlyoffice

---
+++ apps-other/onlyoffice/uninstall.sh
@@ -0,0 +1,7 @@
+sudo apt purge -y --autoremove onlyoffice-desktopeditors
+
+sudo rm -r ~/.config/onlyoffice
+
+sudo rm /etc/apt/sources.list.d/onlyoffice.list
+
+sudo rm /usr/share/keyrings/onlyoffice.gpg
