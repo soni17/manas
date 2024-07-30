f8bbe67a6e268416f436a148908f93df2fa3dc9d
Enderson Isai <elb004@gmail.com>
2024-07-30 01:56:26 -0400

    uninstaller for librewolf

--- apps-other/librewolf/install.sh
+++ apps-other/librewolf/install.sh
@@ -2,11 +2,9 @@
 
 # dependencies
 sudo apt install -y \
-  wget \
   gnupg \
   lsb-release \
-  apt-transport-https \
-  ca-certificates
+  apt-transport-https
 
 # add repository
 distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
---
+++ apps-other/librewolf/uninstall.sh
@@ -0,0 +1,7 @@
+sudo apt purge -y --autoremove librewolf
+
+sudo rm -rf ~/.librewolf
+
+sudo rm -f /etc/apt/sources.list.d/librewolf.sources
+
+sudo rm -f /usr/share/keyrings/librewolf.gpg
