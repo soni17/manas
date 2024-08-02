8399c1bb05caa125ffa29989c83e6454f8311d2d
Enderson Isai <elb004@gmail.com>
2024-08-02 17:58:12 -0400

    fix uninstaller for android-debloater

--- apps-development/universal-android-debloater/uninstall.sh
+++ apps-development/universal-android-debloater/uninstall.sh
@@ -2,6 +2,6 @@
 sudo apt-get purge -y --autoremove adb
 
 # uninstall universal-android-debloater-ng
-sudo rm -f /usr/bin/uad-ng/uad-ng-linux
+sudo rm -f /usr/bin/uad-ng
 sudo rm -f /usr/share/icons/hicolor/256x256/apps/uad-ng.png
 sudo rm -f /usr/share/applications/universal-android-debloater-ng.desktop
