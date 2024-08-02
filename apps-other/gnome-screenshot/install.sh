0f5682559d8451de7e444e186081f5a0838b8b6a
Enderson Isai <elb004@gmail.com>
2024-08-02 19:04:04 -0400

    save screenshots in desktop by default

--- apps-other/gnome-screenshot/install.sh
+++ apps-other/gnome-screenshot/install.sh
@@ -1,3 +1,7 @@
 # https://gitlab.gnome.org/GNOME/gnome-screenshot
 
 sudo apt-get install -y gnome-screenshot
+
+# save screenshots in desktop
+dir=file://$HOME/Desktop
+gsettings set org.gnome.gnome-screenshot last-save-directory "'$dir'"
