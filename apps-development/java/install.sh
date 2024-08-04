e5b79ca0522c13d21e1042a4dd10e945899a5d0b
Enderson Isai <elb004@gmail.com>
2024-08-04 04:14:30 -0400

    do not log output of java installer because it's not useful and it's noisy

--- apps-development/java/install.sh
+++ apps-development/java/install.sh
@@ -4,5 +4,5 @@
 # install latest version of Java (openjdk)
 asdf plugin add java
 OPENJDK=$(asdf list all java | grep openjdk | grep -v + | tail -1)
-asdf install java $OPENJDK
+asdf install java $OPENJDK &> /dev/null
 asdf global java $OPENJDK
