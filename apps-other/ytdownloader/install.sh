4e0b08d4ff9fe4a58e3c06a74cb3741f74a9cdc4
Enderson Isai <elb004@gmail.com>
2024-08-25 10:25:33 -0400

    modify filename for ytdownloader installer

--- apps-other/ytdownloader/install.sh
+++ apps-other/ytdownloader/install.sh
@@ -2,7 +2,7 @@
 
 # download and install
 LATEST_VERSION=$(curl -s "https://api.github.com/repos/aandrew-me/ytDownloader/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-FILENAME="YTDownloader_Linux_amd64.deb"
+FILENAME="YTDownloader_Linux.deb"
 wget -q https://github.com/aandrew-me/ytDownloader/releases/download/${LATEST_VERSION}/$FILENAME -O /tmp/$FILENAME
 sudo apt-get install -y /tmp/$FILENAME
 rm /tmp/$FILENAME
