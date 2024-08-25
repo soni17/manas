d2469311efd6c19de6d2599b70c3cf8d09b2375d
Enderson Isai <elb004@gmail.com>
2024-08-25 12:16:10 -0400

    remove manas if it's already there during installation

--- boot.sh
+++ boot.sh
@@ -1,6 +1,9 @@
 # install dependency
 sudo apt-get install -y git &> /dev/null
 
+# remove if it's already there
+rm -rf ~/.local/share/manas
+
 # download repo
 echo "downloading Manas..."
 git clone git@github.com:soni17/manas.git ~/.local/share/manas &> /dev/null
