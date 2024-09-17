2e61a9b13ef81ddf4dce0247901671579d6b2261
Enderson Isai <elb004@gmail.com>
2024-09-17 16:37:31 -0400

    modify filename for android-debloater installer

--- apps-development/universal-android-debloater/install.sh
+++ apps-development/universal-android-debloater/install.sh
@@ -5,11 +5,10 @@ sudo apt-get install -y adb
 
 # install universal-android-debloater-ng
 LATEST_VERSION=$(curl -s "https://api.github.com/repos/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-FILENAME="uad-ng-linux.zip"
+FILENAME="uad-ng-linux"
 wget -q https://github.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/download/${LATEST_VERSION}/$FILENAME -O /tmp/$FILENAME
-unzip /tmp/$FILENAME -d /tmp
-chmod +x /tmp/uad-ng-linux
-sudo cp /tmp/uad-ng-linux /usr/bin/uad-ng
+chmod +x /tmp/$FILENAME
+sudo mv /tmp/uad-ng-linux /usr/bin
 sudo cp apps-development/universal-android-debloater/uad-ng.png /usr/share/icons/hicolor/256x256/apps
 sudo cp apps-development/universal-android-debloater/universal-android-debloater-ng.desktop /usr/share/applications
-rm /tmp/uad-ng-linux*
+
