2591507d86640642de698bdc4babe74250674a9d
Enderson Isai <elb004@gmail.com>
2024-10-03 16:00:32 -0400

    load sample db for mariadb

--- apps-development/databases/install.sh
+++ apps-development/databases/install.sh
@@ -1,6 +1,9 @@
-# install databases from Debian repo
 sudo apt-get install -y \
-  mariadb-server \
   postgresql \
   redis \
   sqlite3
+
+sudo apt-get install -y mariadb-server
+sudo mariadb -e "create database classicmodels"
+sudo mariadb classicmodels < sampledb.sql
+#m mysql_secure_installation
