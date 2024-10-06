b13571e6b7a3938e1ceecce751fc042b323d6eda
Enderson Isai <elb004@gmail.com>
2024-10-06 17:24:00 -0400

    update installer for librewolf

--- apps-other/librewolf/install.sh
+++ apps-other/librewolf/install.sh
@@ -1,24 +1,9 @@
 # https://librewolf.net/installation/debian/
 
-# remove repository if it's already there
-sudo rm -f /etc/apt/sources.list.d/librewolf.sources
-sudo rm -f /usr/share/keyrings/librewolf.gpg
-
-# add repository
-distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-
-# install
-sudo apt-get update
-sudo apt-get install -y librewolf
+sudo apt-get install extrepo -y
+sudo extrepo enable librewolf
+sudo apt-get update -y
+sudo apt-get install librewolf -y
 
 # config file
 mkdir -p ~/.librewolf
