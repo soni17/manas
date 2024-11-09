d8332de962cd28b048825a2aef4ae4b1c1763e61
Enderson Isai <elb004@gmail.com>
2024-11-09 11:07:58 -0500

    install fsearch from debian repo

--- apps-system/fsearch/uninstall.sh
+++ apps-system/fsearch/uninstall.sh
@@ -1,12 +1,4 @@
-# uninstall only if it's already installed
-if apt search fsearch | grep installed &> /dev/null
-then
-  sudo apt-get purge -y --autoremove fsearch
-fi
+sudo apt-get purge -y --autoremove fsearch
 
 # delete config folder
 sudo rm -rf ~/.config/fsearch
-
-# remove repository
-sudo rm -f /etc/apt/trusted.gpg.d/home_cboxdoerfer.gpg
-sudo rm -f /etc/apt/sources.list.d/home:cboxdoerfer.list
