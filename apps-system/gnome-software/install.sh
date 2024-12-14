c7e5448350e084fe408beb5d9e24777cdd30b31f
Enderson Isai <elb004@gmail.com>
2024-12-14 06:34:02 -0500

    disable auto-updater in gnome-software

--- apps-system/gnome-software/install.sh
+++ apps-system/gnome-software/install.sh
@@ -4,6 +4,10 @@ sudo apt-get install -y \
   gnome-software \
   gnome-software-plugin-flatpak
 
+# disable auto update because package-updater handles it
+dconf write /org/gnome/software/download-updates false
+dconf write /org/gnome/software/download-updates-notify false 
+
 # launcher icon
 mkdir ~/.local/share/applications
 cp apps-system/gnome-software/org.gnome.Software* ~/.local/share/applications
