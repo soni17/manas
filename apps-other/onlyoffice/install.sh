51e31e579fdd588998a26c4461c5ba486a33210c
Enderson Isai <elb004@gmail.com>
2024-12-17 16:49:43 -0500

    use extrepo to add onlyoffice repository

--- apps-other/onlyoffice/install.sh
+++ apps-other/onlyoffice/install.sh
@@ -1,16 +1,7 @@
 # https://helpcenter.onlyoffice.com/installation/desktop-install-ubuntu.aspx
 
-# remove repository if it's already there
-sudo rm -f /etc/apt/sources.list.d/onlyoffice.list
-sudo rm -f /usr/share/keyrings/onlyoffice.gpg
-
 # add repository
-mkdir -p -m 700 ~/.gnupg
-gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
-chmod 644 /tmp/onlyoffice.gpg
-sudo chown root:root /tmp/onlyoffice.gpg
-sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
-echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+sudo extrepo enable onlyoffice-desktopeditors
 
 # install
 sudo apt-get update
