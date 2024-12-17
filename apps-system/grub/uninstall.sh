79b2f2b6394f451e94c5d8c6a87b56c03fe6f816
Enderson Isai <elb004@gmail.com>
2024-12-16 22:46:11 -0500

    uninstaller for grub

--- apps-system/grub/uninstall.sh
+++ apps-system/grub/uninstall.sh
@@ -1,1 +1,5 @@
 sudo apt-get purge -y --autoremove grub-customizer
+
+# reverse settings
+sudo sed -i "s/GRUB_TIMEOUT=0/GRUB_TIMEOUT=5/" /etc/default/grub
+sudo sed -i "s/console=tty12 loglevel=3 vt.global_cursor_default=0/splash/" /etc/default/grub
