53e4718e8cf2a3ec1c0533c7b7e280802c342e69
Enderson Isai <elb004@gmail.com>
2024-12-19 05:26:40 -0500

    more silent boot configs

--- apps-system/grub/install.sh
+++ apps-system/grub/install.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y grub-customizer
 # silent screen at boot
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
 sudo sed -i "s/splash/console=tty12 loglevel=3 vt.global_cursor_default=0/" /etc/default/grub
+sudo sed -i "s/#GRUB_TERMINAL="console"/GRUB_TERMINAL="console"/" /etc/default/grub
 sudo update-grub
