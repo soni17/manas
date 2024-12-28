2f4893ac2b96b3687135865ca747e9f559a90851
Enderson Isai <elb004@gmail.com>
2024-12-28 02:48:02 -0500

    add comments to dbgate installer

--- databases/dbgate/install.sh
+++ databases/dbgate/install.sh
@@ -1,13 +1,17 @@
 # https://dbgate.org/download/
 
-# download
+# installer filename
 FILENAME="dbgate-latest.deb"
+
+# download installer
 wget -q https://github.com/dbgate/dbgate/releases/latest/download/$FILENAME -O /tmp/$FILENAME
 
 # install
 sudo apt-get install -y /tmp/$FILENAME
+
+# delete installer
 sudo rm /tmp/$FILENAME
 
-# copy configs file
+# add config file
 mkdir -p ~/.dbgate
 cp databases/dbgate/settings.json ~/.dbgate
