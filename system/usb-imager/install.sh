3e43546b1c594e64db2d0093def27b6cbd38ad2c
Enderson Isai <elb004@gmail.com>
2024-12-28 02:56:50 -0500

    add more comments

--- system/usb-imager/install.sh
+++ system/usb-imager/install.sh
@@ -1,8 +1,10 @@
 # https://gitlab.com/bztsrc/usbimager/tree/binaries
 # TO DO: download latest version
 
-# download
+# installer filename
 FILENAME="usbimager_1.0.10-amd64.deb"
+
+# download installer
 wget -q https://gitlab.com/bztsrc/usbimager/-/raw/binaries/$FILENAME -O /tmp/$FILENAME
 
 # install
