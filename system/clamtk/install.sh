027d52b8f5fc510f8db1cb126de4ad91d0125ab0
Enderson Isai <elb004@gmail.com>
2024-12-31 11:14:22 -0500

    add clamtk app

---
+++ system/clamtk/install.sh
@@ -0,0 +1,8 @@
+# https://github.com/Cisco-Talos/clamav
+# https://gitlab.com/dave_m/clamtk/-/wikis/Home
+
+# install
+sudo apt-get install -y clamtk
+
+# menu icon overrides
+cp system/clamtk/clamtk* ~/.local/share/applications
---
+++ system/clamtk/uninstall.sh
@@ -0,0 +1,4 @@
+sudo apt-get purge -y --autoremove clamtk
+
+# remove menu icons overrides
+rm -rf ~/.local/share/applications/clamtk*
