4d95e3826f30730f1e2db248aaefb73a2a2e9dce
Enderson Isai <elb004@gmail.com>
2025-01-01 01:34:10 -0500

    require version 12 of debian

--- run-commands/check-os.sh
+++ run-commands/check-os.sh
@@ -12,10 +12,10 @@ version=$VERSION_ID
 desktop=$DESKTOP_SESSION
 
 # stop script if OS is not Debian MATE 12 or higher
-if [ "$distro" != "debian" ] || [ $version -lt 12 ] || [ "$desktop" != "mate" ]; then
+if [ "$distro" != "debian" ] || [[ $version -ne 12 ]] || [ "$desktop" != "mate" ]; then
   echo "$(tput setaf 1)Error: OS requirement not met"
   echo "Installation stopped."
   echo "You are currently running: $distro $version with $desktop desktop"
-  echo "Manas requires: debian 12 or higher with mate desktop"
+  echo "Manas requires: debian 12 with mate desktop"
   exit 1
 fi
