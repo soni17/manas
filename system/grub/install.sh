cf8c8594f7a94e81af3701cd125538aa975dfcfb
Enderson Isai <elb004@gmail.com>
2025-01-12 21:09:05 -0500

    uninstall plymouth to minimize boot screen

--- system/grub/install.sh
+++ system/grub/install.sh
@@ -1,6 +1,9 @@
 # app installed in control center
 sudo apt-get install -y grub-customizer
 
+# remove plymouth
+sudo apt-get purge -y --autoremove plymouth
+
 # silent screen at boot
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
 sudo sed -i "s/splash/console=tty12 loglevel=3 vt.global_cursor_default=0/" /etc/default/grub
