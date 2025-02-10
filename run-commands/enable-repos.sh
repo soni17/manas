014523a46a688575a07d3b6a8f22a6ca38585e26
Enderson Isai <elb004@gmail.com>
2025-02-09 23:18:02 -0500

    refactor mise installer

--- run-commands/enable-repos.sh
+++ run-commands/enable-repos.sh
@@ -14,10 +14,9 @@ sudo apt-get install -y extrepo
 sudo sed -i "s/# - contrib/- contrib/" /etc/extrepo/config.yaml
 sudo sed -i "s/# - non-free/- non-free/" /etc/extrepo/config.yaml
 
-# install mise-en-place
-sudo apt update -y && sudo apt install -y gpg sudo wget curl
+# install mise-en-place (version manager)
 sudo install -dm 755 /etc/apt/keyrings
 wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-sudo apt install -y mise
+sudo apt-get update -y
+sudo apt-get install -y mise
