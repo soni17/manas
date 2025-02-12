562f87f3fa9f093449784c361a9cf7e792d23b76
Enderson Isai <elb004@gmail.com>
2025-02-12 12:33:09 -0500

    audacity replaces tenacity

--- audio-video/tenacity/install.sh
+++
@@ -1,3 +0,0 @@
-# https://tenacityaudio.org
-
-flatpak install flathub -y --system org.tenacityaudio.Tenacity
--- audio-video/tenacity/uninstall.sh
+++
@@ -1,4 +0,0 @@
-if flatpak list | grep org.tenacityaudio.Tenacity
-then
-  flatpak uninstall -y --delete-data org.tenacityaudio.Tenacity
-fi
