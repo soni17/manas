1b53fdd6ad19a3e8548e282aa4fd47dd492d62a9
Enderson Isai <elb004@gmail.com>
2025-03-04 17:08:34 -0500

    fix installer for package-updater

--- system/package-updater/install.sh
+++ system/package-updater/install.sh
@@ -1,6 +1,6 @@
-sudo apt install -y ./gnome-package-updater_43.0-1_amd64.deb
-
 sudo apt-get install -y package-update-indicator
+
+sudo apt install -y ./system/package-updater/gnome-package-updater_43.0-1_amd64.deb
 
 #config file
 mkdir -p ~/.config/package-update-indicator
