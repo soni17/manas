7f11a20a9e49693a43a7c5ec1afae4345aa6d052
Enderson Isai <elb004@gmail.com>
2025-03-04 16:56:51 -0500

    fix docker installer

--- development/docker/install.sh
+++ development/docker/install.sh
@@ -6,9 +6,10 @@ sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-ARCH=$(dpkg --print-architecture)
-DISTRO=$(. /etc/os-release && echo "$VERSION_CODENAME")
-echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $DISTRO stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
 # install docker and extensions
