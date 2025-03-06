b18280aad29a731eae3c2aed65b047bedb61c6a3
Enderson Isai <elb004@gmail.com>
2025-03-06 01:56:32 -0500

    comment out package-updater

--- system/package-updater/install.sh
+++ system/package-updater/install.sh
@@ -1,7 +1,11 @@
-sudo apt-get install -y package-update-indicator
+<<comment
 
-sudo apt install -y ./system/package-updater/gnome-package-updater_43.0-1_amd64.deb
+sudo apt-get install -y \
+  gnome-package-updater \
+  package-update-indicator
 
 #config file
 mkdir -p ~/.config/package-update-indicator
 cp system/package-updater/package-update-indicator.conf ~/.config/package-update-indicator
+
+comment
