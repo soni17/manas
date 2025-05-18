2ba389aab27480394c6801916db9dc680d1192b1
Enderson Isai <elb004@gmail.com>
2025-05-18 13:59:32 -0400

    configure package-updater

--- system/package-updater/install.sh
+++ system/package-updater/install.sh
@@ -1,11 +1,8 @@
-<<comment
+sudo apt-get install -y package-update-indicator
 
-sudo apt-get install -y \
-  gnome-package-updater \
-  package-update-indicator
-
-#config file
+# config file
 mkdir -p ~/.config/package-update-indicator
 cp system/package-updater/package-update-indicator.conf ~/.config/package-update-indicator
 
-comment
+# updates script
+sudo cp system/package-updater/updates /usr/bin
