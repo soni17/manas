0aa3e1546a700f002c03a983bc47da1d690ad9f4
Enderson Isai <elb004@gmail.com>
2025-05-18 13:30:29 -0400

    lock pinta to version 2.1.2

--- graphics/pinta/install.sh
+++ graphics/pinta/install.sh
@@ -1,8 +1,10 @@
 # https://www.pinta-project.com
 # https://github.com/PintaProject/Pinta
 
-# install
+# install pinta 2.1.2 and lock version
 flatpak install flathub -y --system com.github.PintaProject.Pinta
+sudo flatpak update -y --commit=eb978f6ae82fc380b1c6b3a1ed7253a9f8f75115798274c4a71168ecc335ae9c com.github.PintaProject.Pinta
+sudo flatpak mask com.github.PintaProject.Pinta
 
 # settings file
 mkdir -p ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
