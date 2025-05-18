6c104d25c1717883022f512c1309318e60a972c5
Enderson Isai <elb004@gmail.com>
2025-05-18 14:36:11 -0400

    refactor stacer uninstaller

--- system/stacer/uninstall.sh
+++ system/stacer/uninstall.sh
@@ -1,1 +1,5 @@
-sudo apt-get purge -y --autoremove stacer
+# uninstall only if it's already installed
+if apt search stacer | grep installed &> /dev/null
+then
+  sudo apt-get purge -y --autoremove stacer
+fi
