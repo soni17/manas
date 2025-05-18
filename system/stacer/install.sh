d9d301b3e1b297531a072d6b1af56afc42232b36
Enderson Isai <elb004@gmail.com>
2025-05-18 13:48:55 -0400

    add stacer app

---
+++ system/stacer/install.sh
@@ -0,0 +1,10 @@
+# https://oguzhaninan.github.io/Stacer-Web/
+# https://github.com/oguzhaninan/Stacer
+
+# install
+sudo apt-get install -y ./stacer_1.1.0_amd64.deb
+
+# launcher icon that fixes slow startup
+mkdir -p ~/.local/share/applications
+cp system/stacer/stacer.desktop ~/.local/share/applications
+cp system/stacer/stacer-1.desktop ~/.local/share/applications
