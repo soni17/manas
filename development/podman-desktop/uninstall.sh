a9b3c2922e9884e6287a6f8d4e02d2b1ccae0dc9
Enderson Isai <elb004@gmail.com>
2025-05-19 21:03:33 -0400

    delete configs for podman desktop

--- development/podman-desktop/uninstall.sh
+++ development/podman-desktop/uninstall.sh
@@ -2,3 +2,6 @@ if flatpak list | grep io.podman_desktop.PodmanDesktop
 then
   flatpak uninstall -y --delete-data io.podman_desktop.PodmanDesktop
 fi
+
+# delete config folder
+sudo rm -rf ~/.config/Podman Desktop
