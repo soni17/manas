bcc4e4ea560f74575eaa764bda46093aad5b4699
Enderson Isai <elb004@gmail.com>
2025-05-21 20:41:11 -0400

    fix updater for dbgate and beekeeper-studio

--- development/beekeeper-studio/update.sh
+++ development/beekeeper-studio/update.sh
@@ -5,4 +5,4 @@ installed_version=$(apt info beekeeper-studio | grep Version | cut -d ' ' -f 2)
 latest_version=$(curl -s "https://api.github.com/repos/beekeeper-studio/beekeeper-studio/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 latest_version=${latest_version:1}
 
-updater $installed_version $latest_version "databases/beekeeper-studio/install.sh" "beekeeper-studio"
+updater $installed_version $latest_version "development/beekeeper-studio/install.sh" "beekeeper-studio"
