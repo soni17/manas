e45b0313a995121f9ded5d56df4bcb51abff6302
Enderson Isai <elb004@gmail.com>
2025-05-22 00:27:13 -0400

    fix httptoolkit updater

--- development/httptoolkit/update.sh
+++ development/httptoolkit/update.sh
@@ -5,4 +5,4 @@ installed_version=$(apt info httptoolkit | grep Version | cut -d ' ' -f 2)
 latest_version=$(curl -s "https://api.github.com/repos/httptoolkit/httptoolkit-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 latest_version=${latest_version:1}
 
-updater $installed_version $latest_version "direct-download-apps/httptoolkit/install.sh" "HttpToolkit"
+updater $installed_version $latest_version "development/httptoolkit/install.sh" "HttpToolkit"
