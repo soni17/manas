14024b2e29b5fe8a0363c8b00b7597d6d4e82e37
Enderson Isai <elb004@gmail.com>
2025-05-21 21:20:02 -0400

    remove deprecated code

--- run-commands/uninstall-all.sh
+++ run-commands/uninstall-all.sh
@@ -1,6 +1,5 @@
 for uninstaller in accessories/*/uninstall.sh; do source $uninstaller; done
 for uninstaller in audio-video/*/uninstall.sh; do source $uninstaller; done
-for uninstaller in databases/*/uninstall.sh; do source $uninstaller; done
 for uninstaller in development/*/uninstall.sh; do source $uninstaller; done
 for uninstaller in graphics/*/uninstall.sh; do source $uninstaller; done
 for uninstaller in office/*/uninstall.sh; do source $uninstaller; done
--- run-commands/upgrade-os.sh
+++ run-commands/upgrade-os.sh
@@ -1,4 +1,3 @@
 sudo apt-get update -y
-sudo apt-get full-upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt-get upgrade -y
 sudo apt-get autoremove -y
