41a564539de3ea2040e7c4fe643fc8538273dc9c
Enderson Isai <elb004@gmail.com>
2025-06-27 00:20:28 -0400

    enable idiomatic-version-file for ruby,python and node

--- run-commands/enable-repos.sh
+++ run-commands/enable-repos.sh
@@ -19,3 +19,6 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt-get update -y
 sudo apt-get install -y mise
+mise settings add idiomatic_version_file_enable_tools ruby
+mise settings add idiomatic_version_file_enable_tools python
+mise settings add idiomatic_version_file_enable_tools node
