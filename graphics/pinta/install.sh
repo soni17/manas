f58084fa1be69030b75b0af78c007503cdf10eab
Enderson Isai <elb004@gmail.com>
2025-06-26 23:29:02 -0400

    install latest version of pinta

--- graphics/pinta/install.sh
+++ graphics/pinta/install.sh
@@ -1,10 +1,8 @@
 # https://www.pinta-project.com
 # https://github.com/PintaProject/Pinta
 
-# install pinta 2.1.2 and lock version
+# install
 flatpak install flathub -y --system com.github.PintaProject.Pinta
-sudo flatpak update -y --commit=eb978f6ae82fc380b1c6b3a1ed7253a9f8f75115798274c4a71168ecc335ae9c com.github.PintaProject.Pinta
-sudo flatpak mask com.github.PintaProject.Pinta
 
 # add config file
 mkdir -p ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
