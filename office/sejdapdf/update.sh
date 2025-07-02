6195396d95d7c266762b252228a99da9ba8451eb
Enderson Isai <elb004@gmail.com>
2025-07-01 20:34:28 -0400

    fix sejdapdf updater

--- office/sejdapdf/update.sh
+++ office/sejdapdf/update.sh
@@ -1,6 +1,6 @@
 source run-commands/updater.sh
 
 installed_version=$(apt info sejda-desktop | grep Version | cut -d ' ' -f 2)
-latest_version=$(curl https://www.sejda.com/desktop | grep linux | grep -o [0-9].[0-9].[0-9])
+latest_version=$(curl https://www.sejda.com/desktop | grep amd64.deb | grep -o [0-9].[0-9].[0-9])
 
 updater $installed_version $latest_version "office/sejdapdf/install.sh" "SejdaPDF"
