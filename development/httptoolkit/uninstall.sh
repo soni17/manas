08ed0e55d34716c518cbeccfc7beeee0857040e0
Enderson Isai <elb004@gmail.com>
2025-08-11 00:24:01 -0400

    delete panel icons when uninstalling app

--- development/httptoolkit/uninstall.sh
+++ development/httptoolkit/uninstall.sh
@@ -3,3 +3,6 @@ sudo apt-get purge -y --autoremove httptoolkit
 
 # delete config folder
 sudo rm -rf ~/.config/httptoolkit
+
+# delete launcher icon
+rm ~/.local/share/applications/httptoolkit*
--- system/bottles/uninstall.sh
+++ system/bottles/uninstall.sh
@@ -1,1 +1,5 @@
+# uninstall
 flatpak uninstall -y --delete-data com.usebottles.bottles
+
+# delete launcher icon
+rm ~/.local/share/applications/com.usebottles.bottles*
