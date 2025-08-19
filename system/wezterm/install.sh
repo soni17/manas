f9099af9873871a2fff5ebfcfed870445de92418
Enderson Isai <elb004@gmail.com>
2025-08-18 22:24:45 -0400

    fix comment

--- system/wezterm/install.sh
+++ system/wezterm/install.sh
@@ -1,6 +1,6 @@
 # https://wezterm.org/
 
-# app repository
+# add repository
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
