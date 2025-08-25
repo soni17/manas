c0ac59227f6e360a90cc162db5839ffe5dd9be26
Enderson Isai <elb004@gmail.com>
2025-08-24 20:28:01 -0400

    enable all debian repos

--- run-commands/enable-repos.sh
+++ run-commands/enable-repos.sh
@@ -1,17 +1,18 @@
 # enable all debian repositories
 if [ -f "/etc/apt/sources.list" ]; then
+sudo chown $USER:root /etc/apt/sources.list
 cat << EOF > /etc/apt/sources.list
-  deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
-  deb-src http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
 
-  deb http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
-  deb-src http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
 
-  deb http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
-  deb-src http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
 EOF
+sudo chown root:root /etc/apt/sources.list
 fi
-
 sudo apt-get update -y
 
 # add flathub repository
