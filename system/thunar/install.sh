7601e863d6e7720337597d05d712c0f0165cca83
Enderson Isai <elb004@gmail.com>
2025-09-18 00:29:59 -0400

    configure thunar

--- system/thunar/install.sh
+++ system/thunar/install.sh
@@ -1,6 +1,12 @@
 # install
 sudo apt install -y thunar
 
-# add user config files
-mkdir -p ~/.config/thunar
-cp system/thunar/configs/* ~/.config/thunar
+# make default file manager
+xdg-mime default thunar.desktop inode/directory
+
+# add config files
+mkdir -p ~/.config/Thunar
+cp system/thunar/configs/uca.xml ~/.config/Thunar
+
+mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml
+cp system/thunar/configs/thunar.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
