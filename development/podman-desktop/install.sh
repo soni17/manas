40b7b4aa0b12818e85f441860d7bc24bd2116c71
Enderson Isai <elb004@gmail.com>
2025-10-13 14:46:06 -0400

    install podman-desktop from flathub

--- development/podman-desktop/install.sh
+++ development/podman-desktop/install.sh
@@ -4,18 +4,8 @@
 # install podman
 sudo apt-get install -y podman
 
-# get installer filename and latest version
-LATEST_VERSION=$(curl -s "https://api.github.com/repos/podman-desktop/podman-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-FILENAME="podman-desktop-${LATEST_VERSION:1}.flatpak"
-
-# download installer
-wget -q https://github.com/podman-desktop/podman-desktop/releases/download/${LATEST_VERSION}/$FILENAME -O /tmp/$FILENAME
-
 # install
-sudo flatpak install -y /tmp/$FILENAME
-
-# delete installer
-rm /tmp/$FILENAME
+flatpak install flathub -y --system io.podman_desktop.PodmanDesktop
 
 # add config file
 mkdir -p ~/.local/share/containers/podman-desktop/configuration
