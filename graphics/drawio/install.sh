d922ea41830c79cd6fc1d37c3a739acecd51c863
Enderson Isai <elb004@gmail.com>
2025-10-26 14:49:09 -0400

    fix version for drawio because linux not supported in newer versions

--- graphics/drawio/install.sh
+++ graphics/drawio/install.sh
@@ -2,7 +2,8 @@
 # https://github.com/jgraph/drawio-desktop
 
 # get installer filename and latest version
-LATEST_VERSION=$(curl -s "https://api.github.com/repos/jgraph/drawio-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+# LATEST_VERSION=$(curl -s "https://api.github.com/repos/jgraph/drawio-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+LATEST_VERSION=v28.2.5
 FILENAME="drawio-amd64-${LATEST_VERSION:1}.deb"
 
 # download installer
