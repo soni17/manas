d5a64255409de22b25050ec6686f69daa8e2f234
Enderson Isai <elb004@gmail.com>
2025-10-30 00:01:34 -0400

    add pinta config file

--- graphics/pinta/install.sh
+++ graphics/pinta/install.sh
@@ -3,3 +3,7 @@
 
 # install
 flatpak install flathub -y --system com.github.PintaProject.Pinta
+
+# add config file
+mkdir -p ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
+cp graphics/pinta/settings.xml ~/.var/app/com.github.PintaProject.Pinta/config/Pinta
--- graphics/pinta/uninstall.sh
+++ graphics/pinta/uninstall.sh
@@ -2,7 +2,4 @@
 flatpak uninstall -y --delete-data com.github.PintaProject.Pinta
 
 # delete config folder
-sudo rm -rf ~/.config/Pinta
-
-# delete launcher icon
-rm ~/.local/share/applications/com.github.Pinta*
+sudo rm -rf ~/.var/app/com.github.PintaProject.Pinta
