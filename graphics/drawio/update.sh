2b3ecea0a47f3e0d1763e3856ebbc479f01963a9
Enderson Isai <elb004@gmail.com>
2025-11-10 22:22:23 -0500

    add ms-edit app

--- graphics/drawio/update.sh
+++ graphics/drawio/update.sh
@@ -1,9 +1,8 @@
-# source scripts/updater.sh
+source scripts/updater.sh
 
-# installed_version=$(apt info draw.io | grep Version | cut -d ' ' -f 2)
+installed_version=$(apt info draw.io | grep Version | cut -d ' ' -f 2)
 
-# latest_version=$(curl -s "https://api.github.com/repos/jgraph/drawio-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-# latest_version=${latest_version:1}
+latest_version=$(curl -s "https://api.github.com/repos/jgraph/drawio-desktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+latest_version=${latest_version:1}
 
-# updater $installed_version $latest_version "graphics/drawio/install.sh" "drawio"
-echo "no updates"
+updater $installed_version $latest_version "graphics/drawio/install.sh" "drawio"
