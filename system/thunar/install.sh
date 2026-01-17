a6a0f2dd0e3d095f6519f7dddcb3d8491ee09ce8
Enderson Isai <elb004@gmail.com>
2026-01-16 20:23:34 -0500

    refactor thunar installer

--- system/thunar/install.sh
+++ system/thunar/install.sh
@@ -3,7 +3,7 @@ sudo apt install -y thunar
 
 # add config files
 mkdir -p ~/.config/Thunar
-cp system/thunar/configs/uca.xml ~/.config/Thunar
+cp system/thunar/uca.xml ~/.config/Thunar
 
 mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml
-cp system/thunar/configs/thunar.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
+cp system/thunar/thunar.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
