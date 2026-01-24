7b27f333c3583a64051a50836bab9af743fa014f
Enderson Isai <elb004@gmail.com>
2026-01-23 19:53:05 -0500

    refactor updater for fresh-editor

--- development/fresh-editor/update.sh
+++ development/fresh-editor/update.sh
@@ -1,6 +1,6 @@
 source scripts/updater.sh
 
-installed_version=$(fresh -V | cut -d ' ' -f 2)
+installed_version=$(apt info fresh-editor | grep Version | cut -d ' ' -f 2 | cut -d '-' -f 1)
 latest_version=$(curl -s "https://api.github.com/repos/sinelaw/fresh/releases/latest" | grep -Po '"tag_name": "\K[^"]*' | tr -d 'v')
 
 updater $installed_version $latest_version "development/fresh-editor/install.sh" "fresh-editor"
