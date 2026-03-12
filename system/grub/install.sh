87ca77dcefb4e7b3a4a91f0a1ecc33963395bdbb
Enderson Isai <elb004@gmail.com>
2026-03-12 17:17:05 -0400

    no uninstaller for grub settings

--- system/grub/install.sh
+++ system/grub/install.sh
@@ -6,7 +6,7 @@
 
 # silent screen at boot
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
-sudo sed -i "s/splash/quiet splash/" /etc/default/grub
+sudo sed -i "s/splash/quiet splash loglevel=0 systemd.show_status=false vt.global_cursor_default=0/" /etc/default/grub
 sudo update-grub
 
 # use plymouth theme for startup splash screen
