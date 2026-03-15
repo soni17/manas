ff9da0da6e02a88098b12d396f568cb61006a748
Enderson Isai <elb004@gmail.com>
2026-03-15 19:13:56 -0400

    fix devtoys updater

--- development/devtoys/update.sh
+++ development/devtoys/update.sh
@@ -1,6 +1,6 @@
 source scripts/updater.sh
 
-installed_version=$(apt info devtoys | grep Version | cut -d ' ' -f 2)
+installed_version=$(apt info devtoys | grep Version | cut -d ' ' -f 2).0
 latest_version=$(curl -s "https://api.github.com/repos/DevToys-app/DevToys/releases" | grep -Po '"tag_name": "\K[^"]*' | head -n 1  | tr -d 'v')
 
 updater $installed_version $latest_version "development/devtoys/install.sh" "devtoys"
