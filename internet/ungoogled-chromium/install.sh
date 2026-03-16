f35a6adf74a185523c2972459aba45a7ba42aee2
Enderson Isai <elb004@gmail.com>
2026-03-16 12:59:49 -0400

    remove ungoogled-chromium app

--- internet/ungoogled-chromium/install.sh
+++
@@ -1,4 +0,0 @@
-# https://github.com/ungoogled-software/ungoogled-chromium
-
-# install
-flatpak install flathub -y --system io.github.ungoogled_software.ungoogled_chromium
--- internet/ungoogled-chromium/uninstall.sh
+++
@@ -1,5 +0,0 @@
-# uninstall
-flatpak uninstall -y --delete-data io.github.ungoogled_software.ungoogled_chromium
-
-# delete launcher icon
-rm ~/.local/share/applications/io.github.ungoogled_software.ungoogled_chromium.desktop
