ec324b3ef155b9a9317a188354c5dc012cba650e
Enderson Isai <elb004@gmail.com>
2026-03-16 13:05:49 -0400

    remove librewolf app

--- internet/librewolf/uninstall.sh
+++
@@ -1,9 +0,0 @@
-# uninstall
-sudo apt-get purge -y --autoremove librewolf
-
-# delete config folder
-sudo rm -rf ~/.librewolf
-
-# remove repository
-sudo extrepo disable librewolf
-sudo apt-get update -y
