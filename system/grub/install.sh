1ee26646bf79669612157f6218f8a8ec1a3afccf
Enderson Isai <elb004@gmail.com>
2026-03-16 23:47:21 -0400

    update grub settings

--- system/grub/install.sh
+++ system/grub/install.sh
@@ -1,19 +1,16 @@
-# sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
-# sudo sed -i "s/splash/console=tty12 loglevel=3 vt.global_cursor_default=0/" /etc/default/grub
-# sudo sed -i "s/#GRUB_TERMINAL="console"/GRUB_TERMINAL="console"/" /etc/default/grub
-# sudo update-grub
-
-
 # silent screen at boot
 sudo sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
-sudo sed -i "s/splash/quiet splash loglevel=0 systemd.show_status=false vt.global_cursor_default=0/" /etc/default/grub
+sudo sed -i "s/quiet/quiet splash loglevel=0 systemd.show_status=false vt.global_cursor_default=0/" /etc/default/grub
+sudo sed -i "s/#GRUB_TERMINAL="console"/GRUB_TERMINAL="console"/" /etc/default/grub
 sudo update-grub
 
-# use plymouth theme for startup splash screen
+# install plymouth
 sudo apt install -y \
   plymouth \
   plymouth-themes
 
+# add plymouth theme
 sudo cp -r system/grub/debian-mac-style /usr/share/plymouth/themes
 
+# set plymouth theme
 sudo plymouth-set-default-theme --rebuild-initrd debian-mac-style
