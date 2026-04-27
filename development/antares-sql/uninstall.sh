01937cf697243a666085608b0315b7f519b97534
Enderson Isai <elb004@gmail.com>
2026-04-27 18:22:28 -0400

    remove antares app

--- development/antares-sql/uninstall.sh
+++
@@ -1,2 +0,0 @@
-# uninstall
-sudo apt-get purge -y --autoremove antares
--- development/antares-sql/update.sh
+++
@@ -1,6 +0,0 @@
-source scripts/updater.sh
-
-installed_version=$(apt info antares | grep Version | cut -d ' ' -f 2)
-latest_version=$(curl -s "https://api.github.com/repos/antares-sql/antares/releases/latest" | grep -Po '"tag_name": "\K[^"]*' | tr -d 'v')
-
-updater $installed_version $latest_version "development/antares-sql/install.sh" "antares-slq"
