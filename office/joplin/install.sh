4d8576a2a7d30bdb501d1fdf49ebddefddf6797c
Enderson Isai <elb004@gmail.com>
2026-07-04 13:41:19 -0400

    direct-download joplin app

--- office/joplin/install.sh
+++ office/joplin/install.sh
@@ -1,12 +1,18 @@
 # https://joplinapp.org
 # https://github.com/laurent22/joplin
 
-# add repository
-sudo extrepo enable joplin
-sudo apt-get update -y
+# get installer filename and latest version
+LATEST_VERSION=$(curl -s "https://api.github.com/repos/laurent22/joplin/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+FILENAME="joplin-${LATEST_VERSION:1}.deb"
+
+# download installer
+wget -q https://github.com/laurent22/joplin/releases/download/$LATEST_VERSION/$FILENAME -O /tmp/$FILENAME
 
 # install
-sudo apt-get install -y joplin
+sudo apt-get install -y /tmp/$FILENAME
+
+# delete installer
+rm /tmp/$FILENAME
 
 # add config file
 mkdir -p ~/.config/joplin-desktop
--- office/joplin/uninstall.sh
+++ office/joplin/uninstall.sh
@@ -1,6 +1,2 @@
 # uninstall
 sudo apt-get purge -y --autoremove joplin
-
-# remove repository
-sudo extrepo disable joplin
-sudo apt update -y
