3fa798bed45ec93dfdb4dc8e48d20e123ad3a984
Enderson Isai <elb004@gmail.com>
2026-07-04 01:46:22 -0400

    install telegram directly

--- internet/telegram/install.sh
+++ internet/telegram/install.sh
@@ -1,5 +1,30 @@
 # https://telegram.org
-# https://flathub.org/en/apps/org.telegram.desktop
+# https://github.com/telegramdesktop/tdesktop
+# no need for updater because it updates itself
 
-# install
-flatpak install flathub -y --system org.telegram.desktop
+# get installer filename and latest version
+LATEST_VERSION=$(curl -s "https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+FILENAME="tsetup.${LATEST_VERSION:1}.tar.xz"
+
+# download tar
+wget -q https://github.com/telegramdesktop/tdesktop/releases/download/${LATEST_VERSION}/$FILENAME -O /tmp/$FILENAME
+
+# extract tar in tmp folder
+tar -xf /tmp/$FILENAME -C /tmp
+
+# create app folder in /opt (delete existing first)
+sudo rm -rf /opt/Telegram
+sudo mkdir -p /opt/Telegram
+
+# move app files from /tmp to /opt
+sudo mv /tmp/Telegram /opt
+
+# run app so that it completes setup
+/opt/Telegram/Telegram &
+sleep 2
+
+# close app
+pkill -f /opt/Telegram/Telegram
+
+# delete downloaded file
+rm /tmp/$FILENAME
--- internet/telegram/uninstall.sh
+++ internet/telegram/uninstall.sh
@@ -1,1 +1,4 @@
-flatpak uninstall -y --delete-data org.telegram.desktop
+sudo rm -rf /opt/Telegram
+sudo rm ~/.local/share/applications/org.telegram.desktop*
+sudo rm ~/.local/share/icons/hicolor/symbolic/apps/org.telegram*
+sudo rm ~/.local/share/icons/hicolor/256x256/apps/org.telegram*
