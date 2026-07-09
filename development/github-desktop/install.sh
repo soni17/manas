9d3d7a11afce6f5b818564d693c66d7160b6ec73
Enderson Isai <elb004@gmail.com>
2026-07-08 20:52:55 -0400

    Github-Desktop-Plus replaces Github-Desktop

--- development/github-desktop/install.sh
+++ development/github-desktop/install.sh
@@ -1,3 +1,8 @@
-# https://github.com/shiftkey/desktop
+# https://desktop-plus.org/
+# https://github.com/desktop-plus/desktop-plus
 
-flatpak install flathub -y --system io.github.shiftey.Desktop
+flatpak install flathub -y --system io.github.pol_rivero.github-desktop-plus
+
+# override launcher to change app name to Github Desktop Plus
+mkdir -p ~/.local/share/applications
+cp development/github-desktop/io.github.pol* ~/.local/share/applications
--- development/github-desktop/uninstall.sh
+++ development/github-desktop/uninstall.sh
@@ -1,1 +1,5 @@
-flatpak uninstall -y --delete-data io.github.shiftey.Desktop
+# uninstall
+flatpak uninstall -y --delete-data io.github.pol_rivero.github-desktop-plus
+
+# delete launcher icon
+rm ~/.local/share/applications/io.github.pol*
