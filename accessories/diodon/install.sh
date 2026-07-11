5e3ffa798cb21e0bf25cbbb3c7fb0d5b93f6780c
Enderson Isai <elb004@gmail.com>
2026-07-11 18:05:22 -0400

    remove diodon from menu icons

--- accessories/diodon/install.sh
+++ accessories/diodon/install.sh
@@ -7,3 +7,7 @@ sudo apt-get install -y diodon
 # set configs
 gsettings set net.launchpad.Diodon.clipboard keep-clipboard-content true
 gsettings set net.launchpad.Diodon.plugins active-plugins "['indicator']"
+
+# override launcher to hide icon from start menu
+mkdir -p ~/.local/share/applications
+cp accessories/diodon/diodon.desktop ~/.local/share/applications
