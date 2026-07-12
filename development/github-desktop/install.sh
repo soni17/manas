f81366e79c041528ee568a372178b1127f21012b
Enderson Isai <elb004@gmail.com>
2026-07-12 12:17:40 -0400

    add launcher override for github-desktop

--- development/github-desktop/install.sh
+++ development/github-desktop/install.sh
@@ -2,3 +2,7 @@
 # https://github.com/desktop-plus/desktop-plus
 
 flatpak install flathub -y --system org.desktop_plus.desktop-plus
+
+# override launcher to fix slow startup
+mkdir -p ~/.local/share/applications
+cp development/github-desktop/org.desktop_plus* ~/.local/share/applications
--- development/github-desktop/uninstall.sh
+++ development/github-desktop/uninstall.sh
@@ -1,1 +1,5 @@
+# uninstall
 flatpak uninstall -y --delete-data org.desktop_plus.desktop-plus
+
+# delete launcher override
+rm ~/.local/share/applications/org.desktop_plus*
