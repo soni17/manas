7933be3532cb1ee597d55453b1cd2793a246d935
Enderson Isai <elb004@gmail.com>
2026-07-15 22:53:28 -0400

    ad pi coding agent

--- programming-languages/nodejs/install.sh
+++ programming-languages/nodejs/install.sh
@@ -3,3 +3,9 @@
 
 # install latest version of Node
 mise use --global node@latest
+
+# refresh mise
+eval "$(mise activate bash)"
+
+# install some packages
+npm install -g --ignore-scripts @earendil-works/pi-coding-agent
