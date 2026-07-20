7d267d216ef6642798324974c7340bc163368a67
Enderson Isai <elb004@gmail.com>
2026-07-19 23:29:17 -0400

    add chatgpt webapp

---
+++ ai-tools/chatgpt-webapp/install.sh
@@ -0,0 +1,6 @@
+# add icon
+sudo mkdir -p /usr/share/icons/web-apps
+sudo cp ai-tools/chatgpt-webapp/chatgpt.png /usr/share/icons/web-apps
+
+# add launcher
+cp ai-tools/chatgpt-webapp/chatgpt.desktop ~/.local/share/applications
---
+++ ai-tools/chatgpt-webapp/uninstall.sh
@@ -0,0 +1,2 @@
+rm ~/.local/share/applications/chatgpt.desktop
+sudo rm /usr/share/icons/web-apps/chatgpt.png
