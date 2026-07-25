ee1eaf36dcda8c9b0793e7beeda1c699fbc02202
Enderson Isai <elb004@gmail.com>
2026-07-25 12:46:01 -0400

    add gmail web app

---
+++ office/gmail/install.sh
@@ -0,0 +1,6 @@
+# add icon
+sudo mkdir -p /usr/share/icons/web-apps
+sudo cp office/gmail/gmail.png /usr/share/icons/web-apps
+
+# add launcher
+cp office/gmail/gmail.desktop ~/.local/share/applications
---
+++ office/gmail/uninstall.sh
@@ -0,0 +1,2 @@
+rm ~/.local/share/applications/gmail.desktop
+sudo rm /usr/share/icons/web-apps/gmail.png
