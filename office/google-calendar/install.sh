b9b9d3402d4eca06e337360ffd9466038a7807f0
Enderson Isai <elb004@gmail.com>
2026-07-26 18:37:36 -0400

    add google calendar web app

---
+++ office/google-calendar/install.sh
@@ -0,0 +1,6 @@
+# add icon
+sudo mkdir -p /usr/share/icons/web-apps
+sudo cp office/google-calendar/google-calendar.png /usr/share/icons/web-apps
+
+# add launcher
+cp office/google-calendar/google-calendar.desktop ~/.local/share/applications
---
+++ office/google-calendar/uninstall.sh
@@ -0,0 +1,2 @@
+rm ~/.local/share/applications/google-calendar.desktop
+sudo rm /usr/share/icons/web-apps/google-calendar.png
