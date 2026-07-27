462d0827c12cebe8d53a3da6b59987b4697f3d13
Enderson Isai <elb004@gmail.com>
2026-07-27 10:31:34 -0400

    add whatsapp web app

---
+++ internet/whatsapp/install.sh
@@ -0,0 +1,6 @@
+# add icon
+sudo mkdir -p /usr/share/icons/web-apps
+sudo cp internet/whatsapp/whatsapp.png /usr/share/icons/web-apps
+
+# add launcher
+cp internet/whatsapp/whatsapp.desktop ~/.local/share/applications
---
+++ internet/whatsapp/uninstall.sh
@@ -0,0 +1,2 @@
+rm ~/.local/share/applications/whatsapp.desktop
+sudo rm /usr/share/icons/web-apps/whatsapp.png
