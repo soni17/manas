748b24d4d55f2245e669312cfcc6487162276b0a
Enderson Isai <elb004@gmail.com>
2026-07-29 10:14:54 -0400

    remove slack

--- internet/slack/uninstall.sh
+++
@@ -1,9 +0,0 @@
-# uninstall
-sudo apt-get purge -y --autoremove slack-desktop
-
-# remove repository
-sudo rm /etc/apt/keyrings/slack.gpg
-sudo rm /etc/apt/sources.list.d/slack.list
-
-# delete launcher icon
-rm ~/.local/share/applications/slack.desktop
