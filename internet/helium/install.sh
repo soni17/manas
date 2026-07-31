8c945de833af9fa194195303d7f521bd81be1288
Enderson Isai <elb004@gmail.com>
2026-07-31 08:51:35 -0400

    update butterrepo

--- internet/helium/install.sh
+++ internet/helium/install.sh
@@ -2,8 +2,8 @@
 # https://codeberg.org/justaguylinux/butterrepo
 
 # add repository
-curl -fsSL https://justaguylinux.codeberg.page/butterrepo/key.asc | sudo gpg --dearmor -o /usr/share/keyrings/butterrepo.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/butterrepo.gpg] https://justaguylinux.codeberg.page/butterrepo stable main" | sudo tee /etc/apt/sources.list.d/butterrepo.list
+curl -fsSL https://apt.justaguy.dev/key.asc | sudo gpg --dearmor -o /usr/share/keyrings/butterrepo.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/butterrepo.gpg] https://apt.justaguy.dev stable main" | sudo tee /etc/apt/sources.list.d/butterrepo.list
 
 # install
 sudo apt-get update -y
