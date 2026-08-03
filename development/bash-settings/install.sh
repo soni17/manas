ea4ce4405cec3998ef3fba049b65d016474286b4
Enderson Isai <elb004@gmail.com>
2026-08-03 18:34:40 -0400

    add starship to root user .bashrc

--- development/bash-settings/install.sh
+++ development/bash-settings/install.sh
@@ -1,3 +1,7 @@
 cp development/bash-settings/bashrc ~/.bashrc
 cp development/bash-settings/bash-aliases ~/.bash_aliases
 cp development/bash-settings/starship.toml ~/.config
+
+# add starship to root user
+sudo cp development/bash-settings/bashrc-root /root/.bashrc
+sudo cp development/bash-settings/starship.toml /root/.config
