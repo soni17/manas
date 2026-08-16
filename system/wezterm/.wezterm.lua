82573c7f61029dab67c480cd0416b622a770f419
Enderson Isai <elb004@gmail.com>
2026-08-16 17:40:13 -0400

    more theme changes

--- system/wezterm/.wezterm.lua
+++ system/wezterm/.wezterm.lua
@@ -5,17 +5,17 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- initial geometry for new windows:
-config.initial_cols = 109
-config.initial_rows = 27
+config.initial_cols = 95
+config.initial_rows = 26
 
 -- changing the font
-config.font_size = 11
+config.font_size = 12
 
 -- cursor
 config.default_cursor_style = 'SteadyUnderline'
 
 -- background opacity
-config.window_background_opacity = 0.94
+config.window_background_opacity = 0.92
 
 -- scrollbar
 config.enable_scroll_bar = true
@@ -31,7 +31,14 @@ config.colors = {
   cursor_border = 'white'
 }
 
--- config.hide_tab_bar_if_only_one_tab = true
+config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 
 -- Finally, return the configuration to wezterm:
 return config
