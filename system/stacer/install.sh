c2265ae3f0d2fe0e5074fb916bc05a5601ca5298
Enderson Isai <elb004@gmail.com>
2026-09-08 22:26:42 -0400

    remove stacer app

--- system/stacer/install.sh
+++
@@ -1,9 +0,0 @@
-# https://stacer.quentium.fr
-# https://github.com/QuentiumYT/Stacer
-
-# install
-sudo apt-get install -y stacer
-
-# override launcher
-mkdir -p ~/.local/share/applications
-cp system/stacer/stacer.desktop ~/.local/share/applications
--- system/stacer/uninstall.sh
+++
@@ -1,5 +0,0 @@
-# uninstall
-sudo apt-get purge -y --autoremove stacer
-
-# delete config folder
-sudo rm -rf ~/.config/stacer
